427f2e1440a336b9324382cd2e87e0da518a5b66
shypasha <pavel.shyrko@gmail.com>
2017-03-31 12:08:47 +0300

    Initial architecture

---
+++ src/Chmoky.Database/dbo/Stored Procedures/SelectMessagesByAthor.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		P.Shyrko
+-- Create date: 2017-03-31
+-- =============================================
+CREATE PROCEDURE SelectMessagesByAthor
+	@author nvarchar(max),
+	@startdate datetime,
+	@enddate datetime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT 
+	   [id]
+      ,[author]
+      ,[from_dispname]
+      ,[timestamp]
+      ,[edited_by]
+      ,[edited_timestamp]
+      ,[type]
+      ,[body_xml]
+      ,[identities]
+  FROM 
+       [dbo].[Messages]
+  WHERE
+      [author] LIKE @author
+	  AND [timestamp] BETWEEN @startdate AND @enddate
+
+END
---
+++ src/Chmoky.Database/dbo/Tables/Messages.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Messages] (
+    [id]               BIGINT         NOT NULL,
+    [author]           NVARCHAR (MAX) NULL,
+    [from_dispname]    NVARCHAR (MAX) NULL,
+    [timestamp]        DATETIME       NULL,
+    [edited_by]        NVARCHAR (MAX) NULL,
+    [edited_timestamp] DATETIME       NULL,
+    [type]             INT            NULL,
+    [body_xml]         NVARCHAR (MAX) NULL,
+    [identities]       NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
