f8352d5b1023d0c4b8c7a119e7ef51a72fba4d34
Pavel Shyrko <pavel.shyrko@gmail.com>
2017-04-03 17:11:47 +0300

    DB Changes

---
+++ src/Chmoky.Database/dbo/Stored Procedures/FindAuthor.sql
@@ -0,0 +1,31 @@
+﻿
+
+-- =============================================
+-- Author:		P.Shyrko
+-- Create date: 2017-04-03
+-- =============================================
+CREATE PROCEDURE [dbo].[FindAuthor]
+	@Input nvarchar(255)
+AS
+BEGIN
+
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	DECLARE @Input1 nvarchar(257) = '%' + @Input + '%'
+
+	SELECT
+		*
+	FROM
+		[dbo].[SkypeUsers]
+	WHERE
+		@Input IS NULL 
+		OR ([FirstName] LIKE @Input1)
+		OR ([LastName] LIKE @Input1)
+		OR ([author] LIKE @Input1)
+		OR ([account] LIKE @Input1)
+	ORDER BY [LastName], [FirstName] 
+
+
+END
--- src/Chmoky.Database/dbo/Tables/SkypeUsers.sql
+++ src/Chmoky.Database/dbo/Tables/SkypeUsers.sql
@@ -5,9 +5,17 @@
     [DispName]          NVARCHAR (256) NOT NULL,
     [CompanyDepartment] NVARCHAR (100) NULL,
     [JobTitle]          NVARCHAR (255) NULL,
-    [MobilePhone]       NVARCHAR (255) NULL,
+    [Dismissed]         BIT            NULL,
+    [City]              NVARCHAR (255) NULL,
+    [Country]           NVARCHAR (255) NULL,
+    [Account]           NVARCHAR (255) NULL,
+    [EmailDisplayName]  NVARCHAR (255) NULL,
+    [ManagersName]      NVARCHAR (255) NULL,
+    [OfficeLocation]    NVARCHAR (255) NULL,
     CONSTRAINT [PK_SkypeUsers] PRIMARY KEY CLUSTERED ([author] ASC)
 );
+
+
 
 
 
